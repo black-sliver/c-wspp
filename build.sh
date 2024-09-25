c84e9d7f7fb31ec8180e9efd8cecc6b051e29083
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-09-25 22:57:04 +0200

    maybe

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ if needs_build $DEST; then
     echo "Building lib for $PLATFORM ($DUMPMACHINE)"
     set -x
     g++ -o "$DEST" \
-        -shared -fpic $OPTIMZE \
+        -shared $OPTIMZE \
         -Wno-deprecated-declarations \
         src/c-wspp.cpp \
         -Iinclude -Isubprojects/websocketpp -Isubprojects/asio/include \
